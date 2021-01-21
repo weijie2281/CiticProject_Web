c11368e3878efa845fba2f7a4db6c48dfbfac935
FishZheng <1278852593@qq.com>
2021-01-21 16:52:04 +0800

    交易模块页面设计

---
+++ src/components/transaction/TradeMain.vue
@@ -0,0 +1,279 @@
+<template>
+  <div class="transaction">
+    <!-- 搜索功能 -->
+    <Trade-search
+      class="searchBar"
+      @onSearch="searchResult"
+      @onReset="resetResult"
+      @onExport="exportExcel"
+      ref="searchBar"/>
+    <!--  交易细节  -->
+    <div class="TradeTable">
+      <el-table
+        :data="tradeData"
+        border
+        :default-sort="{prop: 'time',order: 'descending'}"
+      >
+        <el-table-column label="序号" type="index" width="70px" align="center"/>
+        <el-table-column label="交易流水号" prop="tradeNum" width="300px" align="center"/>
+        <el-table-column label="转入账号" prop="tradeInAccNum" width="201px" align="center"/>
+        <el-table-column label="转出账号" prop="tradeOutAccNum" width="201px" align="center"/>
+        <el-table-column label="交易金额" prop="tradeMoney" width="140px" align="center"/>
+        <!--<el-table-column label="交易描述" prop="tradeDescription" width="150px" align="center">
+          <template slot-scope="scope">
+            <el-tooltip :content="scope.row.tradeDescription" placement="top" popper-class="tooltip">
+              <div class="abbreviation">
+                {{scope.row.tradeDescription}}
+              </div>
+            </el-tooltip>
+          </template>
+        </el-table-column>-->
+        <el-table-column label="交易状态" prop="tradeStatus" width="100px" align="center"/>
+        <el-table-column label="交易时间" prop="tradeTime" width="120px" align="center" sortable/>
+      </el-table>
+      <div class="page">
+        <el-pagination
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-size="pageSize"
+          :total="tradeData.length">
+        </el-pagination>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TradeSearch from "./TradeSearch";
+
+  export default {
+    name: "TradeMain",
+    components: {TradeSearch},
+    data() {
+      return {
+        // 金额范围
+        minMoney: 0.00,
+        maxMoney: 999999999.99,
+        // 页码
+        currentPage: 1,
+        pageSize: 5,
+        // 查询后返回的数据，后续替换为tradeData
+        transitionForms: [],
+        // 表格中的数据
+        tradeData: [
+          // 交易流水号，转入转出账号，交易金额，交易时间，交易状态
+          // tradeNum、tradeInAccNum、tradeOutAccNum、TradeMoney、TradeTime、TradeStatus
+          {
+            tradeNum: '123456789123456789123456789123456789',
+            tradeInAccNum: '12312341234123412341',
+            tradeOutAccNum: '12312341234123412341',
+            tradeMoney: '999999999.00',
+            tradeTime: '2021-01-19',
+            tradeStatus: '成功',
+            tradeDescription: '备注11111111111111111111111111111111111111111111111111111111111111111' +
+              '111111e22222222222222222222221'
+          }
+        ],
+      }
+    },
+    mounted: function () {
+      this.getTradeData();
+    },
+    methods: {
+      getTradeData() {
+        //加载整个页面
+        var _this = this;
+        var data = {
+          currentPage: this.currentPage,
+          pageSize: this.pageSize,
+          tradeAccNum: '',
+          tradeInAccNum: '',
+          tradeOutAccNum: '',
+          startTradeTime: '',
+          endTradeTime: this.dateFormat(Date.now(), 'yyyy-MM-DD'),
+          startMoney: this.minMoney,
+          endMoney: this.maxMoney,
+        };
+        this.axios
+          .post('/trade/query', data)
+          .then(resp => {
+            if (resp && resp.status === 200) {
+              _this.transitionForms = resp.data
+            } else {
+              console.log('----获取数据失败----')
+            }
+          })
+          .catch(function (error) {
+            console.log('getTradeData',error)
+          });
+      },
+      searchResult() {
+        //查询
+        var _this = this;
+        var form = this.$refs.searchBar.form;
+        // 对金额范围进行限制
+        if(form.endMoney&&form.endMoney>_this.maxMoney){
+          this.$message.error(`输入金额超出最大额度`);
+          return false;
+        }
+        if(form.startMoney&&form.startMoney>_this.maxMoney){
+          this.$message.error(`输入金额超出最大额度`);
+          return false;
+        }
+        if(form.startMoney &&  form.endMoney){
+          if(form.startMoney>form.endMoney){
+            this.$message.error(`输入最大金额应大于最小金额`);
+            return false;
+          }
+        }
+        var data = {
+          currentPage: this.currentPage,
+          pageSize: this.pageSize,
+          //交易流水号
+          tradeAccNum: form.tradeAccNum,
+          // 转入账号
+          tradeInAccNum: form.tradeInAccNum,
+          // 转出账号
+          TradeOutAccNum: form.tradeOutAccNum,
+          // 时间范围
+          startTradeTime: form.startTradeTime,
+          endTradeTime: form.endTradeTime ? form.endTradeTime : this.dateFormat(Date.now(), 'yyyy-MM-DD'),
+          // 金额范围
+          startMoney: form.startMoney ? form.startMoney : this.minMoney,
+          endMoney: form.endMoney ? form.endMoney : this.maxMoney,
+        };
+        this.axios
+          .post('/trade/query', data)
+          .then(resp => {
+            if (resp && resp.status === 200) {
+              _this.transitionForms = resp.data
+            } else {
+              console.log('----searchResult false----');
+              this.$message.warning('查询失败');
+            }
+          })
+          .catch(function (error) {
+            console.log('searchResult',error)
+          });
+      },
+      resetResult() {
+        //  重置
+        console.log('重置');
+        this.getTradeData();
+      },
+      exportExcel() {
+        //  导出Excel
+        var _this = this;
+        var form = this.$refs.searchBar.form;
+        if(form.endMoney&&form.endMoney>_this.maxMoney){
+          this.$message.error(`输入金额超出最大额度`);
+          return false;
+        }
+        if(form.startMoney&&form.startMoney>_this.maxMoney){
+          this.$message.error(`输入金额超出最大额度`);
+          return false;
+        }
+        if(form.startMoney &&  form.endMoney){
+          if(form.startMoney>form.endMoney){
+            this.$message.error(`输入最大金额应大于最小金额`);
+            return false;
+          }
+        }
+        var data = {
+          currentPage: this.currentPage,
+          pageSize: this.pageSize,
+          //交易流水号
+          tradeAccNum: form.tradeAccNum,
+          // 转入账号
+          tradeInAccNum: form.tradeInAccNum,
+          // 转出账号
+          TradeOutAccNum: form.tradeOutAccNum,
+          // 时间范围
+          startTradeTime: form.startTradeTime,
+          endTradeTime: form.endTradeTime ? form.endTradeTime : this.dateFormat(Date.now(), 'yyyy-MM-DD'),
+          // 金额范围
+          startMoney: form.startMoney ? form.startMoney : this.minMoney,
+          endMoney: form.endMoney ? form.endMoney : this.maxMoney,
+        };
+        this.axios
+          .post('/excel/tradeExport', data)
+          .then(resp => {
+            if (resp && resp.status === 200) {
+              // _this.transitionForms = resp.data
+              let url = window.URL.createObjectURL(new Blob([resp.data]));
+              let link = document.createElement('a');
+              link.style.display='none';
+              link.href=url;
+              link.setAttribute('download',`交易流水.xlsx`);
+              document.body.appendChild(link);
+              link.click()
+            } else {
+              console.log('----exportExcel false----');
+              this.$message.warning('导出excel失败');
+            }
+          })
+          .catch(function (error) {
+            console.log('exportExcel',error)
+          });
+      },
+      handleCurrentChange: function (currentPage) {
+        //  处理页码的函数
+        this.currentPage = currentPage
+      },
+      dateFormat(date, format) {
+        //  处理时间格式的函数
+        if (date) {
+          const d = new Date(date);
+          const o = {
+            yearData: d.getFullYear(),
+            monthData: d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : d.getMonth() + 1,
+            dateData: d.getDate() < 10 ? `0${d.getDate()}` : d.getDate()
+          };
+          switch (format) {
+            case 'yyyy-MM-DD':
+              return `${o.yearData}-${o.monthData}-${o.dateData}`;
+              break;
+            case 'yyyyMMDD':
+              return `${o.yearData}${o.monthData}${o.dateData}`;
+              break;
+          }
+        } else {
+          return this.$message(`暂无数据`);
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .transaction {
+    background-color: white;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+
+  .searchBar {
+
+  }
+
+  .TradeTable {
+    margin: 20px;
+  }
+
+  .page {
+    text-align: center;
+    margin: auto;
+  }
+
+  .abbreviation {
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    line-clamp: 2;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    /*height: 40px;*/
+    position: relative;
+  }
+</style>
---
+++ src/components/transaction/TradeSearch.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="searchBar">
+    <el-form class="el-form" :rules="ruleForm" ref="form" :model="form" label-width="90px"
+             label-position="left">
+      <el-row :gutter="24">
+        <el-col :span="16">
+          <el-form-item label="交易流水号" prop="tradeNum">
+            <el-input
+              prefix-icon="el-icon-search"
+              v-model="form.tradeNum"
+              placeholder="请输入交易流水号"
+              clearable/>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="24">
+        <el-col :span="12">
+          <el-form-item label="转入账号" prop="tradeInAccNum">
+            <el-input
+              prefix-icon="el-icon-search"
+              v-model="form.tradeInAccNum"
+              placeholder="请输入转入账号"
+              clearable/>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="转出账号" prop="tradeOutAccNum">
+            <el-input
+              prefix-icon="el-icon-search"
+              v-model="form.tradeOutAccNum"
+              placeholder="请输入转出账号"
+              clearable/>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="24">
+        <el-col :span="10">
+          <el-form-item label="交易日期" prop="startTradeTime">
+            <el-date-picker
+              type="date"
+              placeholder="请选择开始日期"
+              v-model="form.startTradeTime"
+              format="yyyy 年 MM 月 dd 日"
+              value-format="yyyy-MM-dd"
+              :picker-options="pickerOptionsStart"
+              style="width: 200px"/>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10">
+          <el-form-item label="至" prop="endTradeTime" label-width="25px">
+            <el-date-picker
+              type="date"
+              placeholder="请选择结束日期"
+              v-model="form.endTradeTime"
+              format="yyyy 年 MM 月 dd 日"
+              value-format="yyyy-MM-dd"
+              :picker-options="pickerOptionsEnd"
+              style="width: 200px"/>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="24">
+        <el-col :span="10">
+          <el-form-item label="金额范围" prop="startMoney">
+            <el-input v-model="form.startMoney"
+                      placeholder="0.00"
+                      clearable
+                      style="width: 200px"/>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10">
+          <el-form-item label="至" prop="endMoney" label-width="25px">
+            <el-input v-model="form.endMoney" placeholder="0.00" clearable style="width: 200px"/>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-form-item>
+        <el-button size="middle" type="primary" icon="el-icon-search" @click="onSearch">搜索</el-button>
+        <el-button size="middle" type="primary" icon="el-icon-delete" @click="onReset">重置</el-button>
+        <!--  excel导出按钮  -->
+        <el-button size="middle" type="primary" @click="onExport" class="ButtonExcel">导出EXCEL</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "TradeSearch",
+    data() {
+      var validateAcc = (rule, value, callback) => {
+        if (/[^\d]/g.test(value)) {
+          this.$refs['form'].resetFields();
+          return callback(new Error('只能输入数字'))
+        }
+      };
+      var validateMoney = (rule, value, callback) => {
+        if (!(/^\d+(\.(\d{0,2}))?$/g.test(value))) {
+          this.$refs['form'].resetFields();
+          return callback(new Error('只能输入整数和两位小数'))
+        }
+      };
+      return {
+        // 查询字段：交易流水号，转入账号，转出账号，交易时间，交易金额
+        form: {
+          tradeNum:'',
+          tradeInAccNum:'',
+          tradeOutAccNum:'',
+          startTradeTime: '',
+          endTradeTime: '',
+          startMoney: '',
+          endMoney: ''
+        },
+        ruleForm: {
+          tradeInAccNum: [
+            {validator: validateAcc, trigger: 'blur'}
+          ],
+          tradeOutAccNum: [
+            {validator: validateAcc, trigger: 'blur'}
+          ],
+          startMoney: [
+            {validator: validateMoney, trigger: 'blur'}
+          ],
+          endMoney: [
+            {validator: validateMoney, trigger: 'blur'}
+          ],
+        },
+        pickerOptionsStart: {
+          disabledDate: time => {
+            let endDateVal = this.form.endTradeTime;
+            let endDate = new Date(endDateVal).getTime();
+            if(endDateVal){
+              return time.getTime()>endDate;
+            }else{
+              return time.getTime()>Date.now();
+            }
+          }
+        },
+        pickerOptionsEnd: {
+          disabledDate: time => {
+            let startDateVal = this.form.startTradeTime;
+            let startDate = new Date(startDateVal).getTime();
+            if(startDateVal){
+              return time.getTime()>Date.now()||time.getTime()<startDate;
+            }else{
+              return time.getTime()>Date.now();
+            }
+          }
+        }
+      }
+    },
+    methods: {
+      onSearch() {
+        // 查询
+        this.$emit('onSearch');
+      },
+      onReset() {
+        // 重置
+        this.$refs['form'].resetFields();
+        this.$emit('onReset')
+      },
+      onExport() {
+        // 导出excel
+        this.$emit('exportExcel')
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .searchBar {
+
+  }
+
+  .el-form {
+    width: 700px;
+    padding: 20px;
+    margin: auto;
+  }
+</style>
